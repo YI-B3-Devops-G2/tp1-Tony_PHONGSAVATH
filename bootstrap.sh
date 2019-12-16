ed3990e748a523522f0f1b600997aaa4cf0d8c71
tonyphg <58935447+tonyphg@users.noreply.github.com>
2019-12-16 15:42:41 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
      sudo apt-get update
-	 sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
-	 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+     sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+     curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
      sudo apt-get install -y nodejs
-	 sudo apt-get install -y nginx
-	 sudo systemctl start nginx
-	 cd /var/www/html
-	 sudo echo "<!DOCTYPE html>
-				<html>
-				<body>
-				B3 Devops - TP 1
-				</body>
-				</html>" > index.nginx-debian.html
-	sudo systemctl restart nginx
+     sudo apt-get install -y nginx
+     sudo systemctl start nginx
+     cd /var/www/html
+     sudo echo "<!DOCTYPE html>
+     <html>
+     <body>
+     B3 Devops - TP 1
+     </body>
+     </html>" > index.nginx-debian.html
+     sudo systemctl restart nginx
